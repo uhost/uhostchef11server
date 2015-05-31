1778ae37601f1cb7e257424cfdc8ebd4b32987b6
Mark Allen <mark@markcallen.com>
2015-05-31 12:03:35 -0700

    Updated to chef-server 11.1.6

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,23 +1,11 @@
 case node['platform']
 when 'ubuntu'
-  #http://opscode-omnibus-packages.s3.amazonaws.com/ubuntu/12.04/x86_64/chef-server_11.0.12-1.ubuntu.12.04_amd64.deb
-  #http://opscode-omnibus-packages.s3.amazonaws.com/ubuntu/12.04/x86_64/chef-server_11.1.6-1_amd64.deb
-  default['chef11server']['os'] = "ubuntu"
-  default['chef11server']['osversion'] = "12.04"
-  default['chef11server']['version'] = "_11.0.12-1.ubuntu.12.04_amd64"
-  default['chef11server']['package'] = "deb"
+  default['chef11server']['download'] = "https://web-dl.packagecloud.io/chef/stable/packages/ubuntu/precise/chef-server_11.1.6-1_amd64.deb"
+  default['chef11server']['filename'] = "chef-server_11.1.6-1_amd64.deb"
 when 'centos'
-  #http://opscode-omnibus-packages.s3.amazonaws.com/el/5/x86_64/chef-server-11.0.12-1.el5.x86_64.rpm
-  default['chef11server']['os'] = "el"
-  default['chef11server']['osversion'] = "5"
-  default['chef11server']['version'] = "-11.0.12-1.el5.x86_64"
-  default['chef11server']['package'] = "rpm"
+  default['chef11server']['download'] = "https://web-dl.packagecloud.io/chef/stable/packages/el/6/chef-server-11.1.6-1.el6.x86_64.rpm"
+  default['chef11server']['filename'] = "chef-server-11.1.6-1.el6.x86_64.rpm"
 end
-
-default['chef11server']['filename'] = "chef-server#{node['chef11server']['version']}.#{node['chef11server']['package']}"
-default['chef11server']['hostname'] = "http://opscode-omnibus-packages.s3.amazonaws.com"
-default['chef11server']['path'] = "#{node['chef11server']['os']}/#{node['chef11server']['osversion']}/x86_64/"
-default['chef11server']['download'] = "#{node['chef11server']['hostname']}/#{node['chef11server']['path']}#{node['chef11server']['filename']}"
 
 default['chef11server']['bootstrap'] = true
 default['chef11server']['notification_email'] = "support@getuhost.org"
