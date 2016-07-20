f620027367198f304679aa1edc514d726e8252f1
Mark C Allen <mark@markcallen.com>
2016-07-19 21:32:53 -0700

    Changed path to download files

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,9 +1,9 @@
 case node['platform']
 when 'ubuntu'
-  default['chef11server']['download'] = "https://web-dl.packagecloud.io/chef/stable/packages/ubuntu/precise/chef-server_11.1.6-1_amd64.deb"
+  default['chef11server']['download'] = "https://packages.chef.io/stable/ubuntu/12.04/chef-server_11.1.6-1_amd64.deb"
   default['chef11server']['filename'] = "chef-server_11.1.6-1_amd64.deb"
 when 'centos'
-  default['chef11server']['download'] = "https://web-dl.packagecloud.io/chef/stable/packages/el/6/chef-server-11.1.6-1.el6.x86_64.rpm"
+  default['chef11server']['download'] = "https://packages.chef.io/stable/el/6/chef-server-11.1.6-1.el6.x86_64.rpm"
   default['chef11server']['filename'] = "chef-server-11.1.6-1.el6.x86_64.rpm"
 end
 
